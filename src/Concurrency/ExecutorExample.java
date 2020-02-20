c892bce21ffaa8e96876403250a3b5a54b7701ca
Ajesh Pai <ajesh.pai@capitalfloat.com>
2020-02-20 12:12:50 +0530

    [+] Executor and ExecutorService examples

---
+++ src/Concurrency/ExecutorExample.java
@@ -0,0 +1,24 @@
+package Concurrency;
+
+import java.util.concurrent.Executor;
+
+public class ExecutorExample
+{
+	public static void main(String[] args)
+	{
+		Invoker invoker = new Invoker();
+		invoker.execute(() -> {
+			System.out.println("Something just trying to do");
+		});
+	}
+
+	public static class Invoker implements Executor
+	{
+		@Override
+		public void execute(Runnable command)
+		{
+			command.run();
+		}
+	}
+}
+
---
+++ src/Concurrency/ExecutorServiceExample.java
@@ -0,0 +1,25 @@
+package Concurrency;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceExample
+{
+	public static class Task implements  Runnable {
+		@Override
+		public void run()
+		{
+			System.out.println("Print something in executor service");
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		ExecutorService executorService = Executors.newScheduledThreadPool(10);
+		executorService.execute(new Task());
+		executorService.awaitTermination(10000, TimeUnit.MILLISECONDS);
+		executorService.shutdownNow();
+	}
+}
