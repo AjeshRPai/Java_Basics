24170d9ebc43a6f63b9ae41ff0a40712b6c81948
Ajesh Pai <ajeshr2k1@gmail.com>
2020-05-13 12:56:27 +0530

    [*] Singleton pattern modification

--- src/DesignPatterns/Creation/SingletonPattern.java
+++ src/DesignPatterns/Creation/SingletonPattern.java
@@ -6,26 +6,72 @@ package DesignPatterns.Creation;
  * https://www.java67.com/2015/09/thread-safe-singleton-in-java-using-double-checked-locking-pattern.html
  * https://www.journaldev.com/171/thread-safety-in-java-singleton-classes-with-example-code
  * http://tutorials.jenkov.com/java-concurrency/volatile.html
- *
- * */
+ * https://javarevisited.blogspot.com/2014/05/double-checked-locking-on-singleton-in-java.html#ixzz6MIrFbSIZ
+ */
 
 class Singleton
 {
-	private static volatile Singleton instance = null;
 
-	private Singleton(){}
+	private volatile static Singleton _instance;
+
+	private Singleton()
+	{
+		// preventing Singleton object instantiation from outside
+	}
+
+	/*
+	 * 1st version: creates multiple instance if two thread access
+	 * this method simultaneously
+	 */
 
 	public static Singleton getInstance()
 	{
-		if(instance == null)
+		if(_instance == null)
+		{
+			_instance = new Singleton();
+		}
+		return _instance;
+	}
+
+	/*
+	 * 2nd version : this definitely thread-safe and only
+	 * creates one instance of Singleton on concurrent environment
+	 * but unnecessarily expensive due to cost of synchronization
+	 * at every call.
+	 */
+
+	public static synchronized Singleton getInstanceTS()
+	{
+		if(_instance == null)
+		{
+			_instance = new Singleton();
+		}
+		return _instance;
+	}
+
+    /*
+     * 3rd version : An implementation of double checked locking of Singleton.
+     * Intention is to minimize cost of synchronization and  improve performance,
+     * by only locking critical section of code, the code which creates
+ instance of Singleton class.
+     * By the way this is still broken, if we don't make _instance volatile,
+ as another thread can
+     * see a half initialized instance of Singleton.
+     */
+
+	public static Singleton getInstanceDC()
+	{
+		if(_instance == null)
 		{
 			synchronized(Singleton.class)
 			{
-				if(instance == null)
-					instance = new Singleton();
+				if(_instance == null)
+				{
+					_instance = new Singleton();
+				}
 			}
 		}
-		return instance;
+		return _instance;
 	}
 }
 
