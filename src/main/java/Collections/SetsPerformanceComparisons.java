93f423abd4aef70e3c7cfa7d9cd00d2eaceb89a9
Ajesh Pai <ajeshr2k1@gmail.com>
2020-09-04 12:22:58 +0530

    [*] BenchMarking improvements

--- src/main/java/Collections/SetsPerformanceComparisons.java
+++ src/main/java/Collections/SetsPerformanceComparisons.java
@@ -13,69 +13,21 @@ import java.util.concurrent.*;
 @Warmup(iterations = 5, time = 1)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Measurement(iterations = 10, time = 1)
-public class SetsExamples {
-
-    private Set<String> hashSet;
-    private Set<String> copyOnArraySet;
-    private Set<String> linkedHashSet;
+public class SetsPerformanceComparisons {
 
     //Sets doesnt allow duplicate Elements
-    //
     public static void main(String[] args) throws IOException, RunnerException {
         org.openjdk.jmh.Main.main(args);
-        //Hash Set
-        // Its implemented by an array.
-        // Hashtable in which position is calculated by hash function of contents
-        Set<String> set1 = new HashSet<>();
-        set1.add("item1");
-        set1.add("item2");
-        set1.add("item3");
-        set1.add("item4");
-        set1.add("item5");
 
-        // Order is not maintained
-        // Iteration will take O(table size)
-        // as the items are stored according to hash set size
-        for (String item : set1) {
-            System.out.println("item = " + item);
-        }
-
-        // Dont give hashtable a size ?
-
-        //Linked Hashset
-        // This class is same as set
-
-        Set<String> set2 = new LinkedHashSet<>();
-        set2.add("item1");
-        set2.add("item2");
-        set2.add("item3");
-        set2.add("item4");
-        set2.add("item5");
-
-        //Order is maintained by having a pointer to the next element
-        // Iteration will take O(element numbers)
-        // as the items are stored according to hash set size
-        for (String item : set2) {
-            System.out.println("item = " + item);
-        }
-
-        //CopyonWriteArraySet
-        Set<String> set3 = new CopyOnWriteArraySet<>();
-        set2.add("item1");
-        set2.add("item2");
-        set2.add("item3");
-        set2.add("item4");
-        set2.add("item5");
-
-        //Order is maintained by having a pointer to the next element
-        // Iteration will take O(element numbers)
-        // as the items are stored according to hash set size
-        for (String item : set2) {
-            System.out.println("item = " + item);
-        }
 
     }
 
+    //Hash Set
+    // Its implemented by an array.
+    // Hashtable in which position is calculated by hash function of contents
+    // Order is not maintained
+    // Iteration will take O(table size)
+    // as the items are stored according to hash set size
     @Benchmark
     public static void hashSetInsertionsWithSize() {
         Set<SomeClass> set = new HashSet<>(1000);
@@ -100,6 +52,11 @@ public class SetsExamples {
         }
     }
 
+    //Linked Hashset
+    // This class is same as set
+    //Order is maintained by having a pointer to the next element
+    // Iteration will take O(element numbers)
+    // as the items are stored according to hash set size
     @Benchmark
     public static void linkedHashSetInsertions() {
         Set<Integer> set = new LinkedHashSet<>();
@@ -108,6 +65,7 @@ public class SetsExamples {
         }
     }
 
+
     @Benchmark
     public static void CopyOnWriteArraySetInsertions() {
         Set<Integer> set = new CopyOnWriteArraySet<>();
@@ -115,6 +73,31 @@ public class SetsExamples {
             set.add(i);
         }
     }
+
+    @Benchmark
+    public static void TreeSetInsertions() {
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < 1000; i++) {
+            set.add(i);
+        }
+    }
+
+    @Benchmark
+    public static void NavigableSetInsertions() {
+        NavigableSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < 1000; i++) {
+            set.add(i);
+        }
+    }
+
+    @Benchmark
+    public static void SortedSetInsertions() {
+        SortedSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < 1000; i++) {
+            set.add(i);
+        }
+    }
+
 
     private static class SomeClass {
         public Integer someInteger;
@@ -125,7 +108,6 @@ public class SetsExamples {
             this.someString = someString;
         }
     }
-
 
     public static Set<Integer> iterateSet(Set<Integer> set) {
         for (Integer integer : set) {
